2e6b8990eb0f2b662b29f54c4baca47837840ce9
Steve Renner <che1111@hotmail.com>
2020-02-14 08:05:36 -0800

    Beginning challenge

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -1,4 +1,4 @@
-# Write classes for the following class hierarchy:
+# Write classes for the following class hierarchy: Starting
 #
 #  [Vehicle]->[FlightVehicle]->[Starship]
 #      |                |
